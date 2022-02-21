52f6e574bfc152fcbc767914695a087164044ef4
Khandoker Tanjim Ahammad <63535729+Khandoker09@users.noreply.github.com>
2022-02-21 13:14:46 +0100

    updated
    -include job tittle
    - remove index
    -separate 1st and last name

--- twitteruserFinder.py
+++ twitteruserFinder.py
@@ -1,9 +1,10 @@
+
 '''
 Purpose: collect users info from twitter by search quary keywords.
 Methods : use tweepy and api key from my delveloper accounts 
 Author : Khandoker Tanjim Ahammad
 published : 2022- 02- 15
-Remakrs : It is using Tkinter Ui to make things easiar for no technical backgrounds people. 
+Remakrs : still need to fix the rate limits 
 
 '''
 import tkinter as tk
@@ -11,20 +12,25 @@ import tweepy
 import time
 from tweepy import OAuthHandler
 import pandas as pd
+import nltk
+from nltk import word_tokenize, pos_tag, pos_tag_sents
+import re
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
 import datetime
 current_date = datetime.datetime.now()
 # root window
 root = tk.Tk()
 root.geometry('500x200')
 root.resizable(0,0)
-root.title('Twitter User Finder Version1.0')
+root.title('Twitter Data GeneratorV1.2')
 
 def my_function():
     #twitter credentials
-    consumer_key = "your_key"
-    consumer_secret = "your_key"
-    access_key = "your_key"
-    access_secret = "your_key"
+    consumer_key = "your api key"
+    consumer_secret = "your api key"
+    access_key = "your api key"
+    access_secret = "your api key"
     # Pass your twitter credentials to tweepy via its OAuthHandler
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
@@ -33,12 +39,13 @@ def my_function():
     api.wait_on_rate_limit_notify = True
     api = tweepy.API(auth, wait_on_rate_limit=True)
     id = my_entry.get()
-    text_query = (id) # query inputs 
-    #limits to 1000 user finder using developer api 
+    text_query = (id)#'managing editor'
     max_tweets = 1000
   
     # Creation of query method using parameters
     tweets = tweepy.Cursor(api.search_users,q=text_query ,count=5,include_entities=True).items(max_tweets)  #
+    
+    # Pulling information from tweets iterable object
     try:
     # Add or remove tweet information you want in the below list comprehension
         tweets_list = [[tweet.name,tweet.screen_name, tweet.id_str,tweet.location, tweet.url, tweet.description, tweet.verified, tweet.followers_count, tweet.friends_count, tweet.statuses_count, tweet.listed_count, tweet.created_at, tweet.profile_image_url_https, tweet.default_profile] for tweet in tweets]
@@ -46,15 +53,20 @@ def my_function():
     except:
         tweets_list = [[tweet.name,tweet.screen_name, tweet.id_str,tweet.location, tweet.url, tweet.description, tweet.verified, tweet.followers_count, tweet.friends_count, tweet.statuses_count, tweet.listed_count, tweet.created_at, tweet.profile_image_url_https, tweet.default_profile] for tweet in tweets]
         time.sleep(10)
-    # make a dataframe to keep all the users information
+    # Did not include column names to simplify code 
     tweets_df = pd.DataFrame(tweets_list)
-    #creating the columns name according to the data
     tweets_df.columns = ['username','user_screenname','userid','userslocation','usersurl','usersdescription','userverified','followerscount','friendscount','statusescount','listedcount','createdat','profileimageurlhttps','defaultrofile']
+    tweets_df[['firstname','lastname']] =   tweets_df['username'].loc[  tweets_df['username'].str.split().str.len() == 2].str.split(expand=True)
+    tweets_df=tweets_df[['username','firstname','lastname','user_screenname','userid','userslocation','usersdescription']]
+    # use regex to figureout the company name  @([\w.-]+)\.
+    tweets_df['employeecompany'] = tweets_df['usersdescription'].str.extract(r'@([\w.-]+)\.', expand=False) 
+    tweets_df=tweets_df[['username','firstname','lastname','employeecompany','user_screenname','userid','userslocation','usersdescription']]
+    #tweets_df['POSTags'] = pos_tag_sents(tweets_df['usersdescription'].apply(word_tokenize).tolist())
     filename = str('twitter_search_list_')+str(current_date.year)+str('_')+str(current_date.month)+str('_')+str(current_date.day)
     print(tweets_df)
-    tweets_df.to_csv(str(filename + '.csv'))
+    tweets_df.to_csv(str(filename + '.csv'),index=False)
+    
 
-# Buiding Buttons and query box for the tkinter user interface
 my_label = tk.Label(root, text = "Enter the twitter query ")
 my_label.grid(row = 0, column = 0)
 my_entry = tk.Entry(root)
